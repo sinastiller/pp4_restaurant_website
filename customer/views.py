201c09ef8a5e285f0111d656aa141a5259aa5297
sinastiller <sinastiller@gmx.de>
2023-06-29 10:33:59 +0000

    Create IndexPage() class in customer > views.py

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, request
+from django.view import view
 
-# Create your views here.
+
+""" Customer will be able to view the landing page"""
+
+
+class IndexPage(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, templates/index.html)

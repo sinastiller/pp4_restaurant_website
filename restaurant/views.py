ca2e9b6587b5792109940d8b9d000af1d02d79c1
sinastiller <sinastiller@gmx.de>
2023-06-30 15:54:14 +0000

    Create ViewMenu class

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Menu, Category
 
-# Create your views here.
+
+class ViewMenu(View):
+    """
+    View menu items and categories
+    """
+
+    def get(self, request, *args, **kwargs):
+        colds = Menu.objects.filter(category__name__contains="Chill 'n Thrill")
+        hots = Menu.objects.filter(
+            category__name__contains='Savoury Temptations')
+        sweets = Menu.objects.filter(
+            category__name__contains='Sweet Tooth Hugs')
+        drinks = Menu.objects.filter(
+            category__name__contains='Liquid Delights')
+
+        context = {
+            'colds': colds,
+            'hots': hots,
+            'sweets': sweets,
+            'drink': drinks,
+        }
+
+        return render(request, '../templates/menu.html', context)

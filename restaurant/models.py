f38307b4fc9dd5a17afbb6c3adf6e3bb121a2b4e
sinastiller <sinastiller@gmx.de>
2023-06-30 15:01:15 +0000

    Create menu model

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    """
+    Add item to menu as admin
+    """
+    category = models.ManyToManyField('Category', related_name='item')
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+        return str(self.price) + ": €"

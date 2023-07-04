fd01ad030f7506ce84a2ba58e55fa3d482fc96e9
sinastiller <sinastiller@gmx.de>
2023-07-04 15:50:32 +0000

    Delete customer app and models,views,urls for menu

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,10 +1,1 @@
 from django.contrib import admin
-from .models import Menu
-# , Category
-
-
-@admin.register(Menu)
-class MenuAdmin(admin.ModelAdmin):
-    search_fields = ['name', 'category']
-    prepopulated_fields = {'slug': ('name',)}
-# admin.site.register(Category)
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,30 +1,5 @@
 from django.shortcuts import render
-from django.views import View
-# from .models import Menu, Category
-
-
-# class ViewMenu(View):
-#     """
-#     View menu items and categories
-#     """
-
-#     def get(self, request, *args, **kwargs):
-#         colds = Menu.objects.filter(category__name__contains="Chill 'n Thrill")
-#         hots = Menu.objects.filter(
-#             category__name__contains='Savoury Temptations')
-#         sweets = Menu.objects.filter(
-#             category__name__contains='Sweet Tooth Hugs')
-#         drinks = Menu.objects.filter(
-#             category__name__contains='Liquid Delights')
-
-#         context = {
-#             'colds': colds,
-#             'hots': hots,
-#             'sweets': sweets,
-#             'drink': drinks,
-#         }
-
-#         return render(request, '../templates/menu.html', context)
+from django.views import generic, View
 
 
 class IndexPage(View):

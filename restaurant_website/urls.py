9f601538b60ffc7cba1bc336b258e529795227ac
sinastiller <sinastiller@gmx.de>
2023-07-05 10:54:29 +0000

    Include path for menu.urls

--- restaurant_website/urls.py
+++ restaurant_website/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from restaurant.views import IndexPage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexPage.as_view(), name='index'),
+    path('menu_list/', include('menu.urls', namespace='menu_list')),
 ]

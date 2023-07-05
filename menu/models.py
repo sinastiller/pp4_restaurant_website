b57b5fe00a0f8982691462d45ef7b00403ea5b41
sinastiller <sinastiller@gmx.de>
2023-07-05 14:05:38 +0000

    Add category model and category as foreignkey to menu model

--- menu/models.py
+++ menu/models.py
@@ -6,6 +6,8 @@ class Menu(models.Model):
     """
     Creates the menu model
     """
+    category = models.ForeignKey(
+        'Category', on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=400)
     price = models.DecimalField(max_digits=4, decimal_places=2)
@@ -20,6 +22,21 @@ class Menu(models.Model):
         if not self.slug and self.name:
             self.slug = slugify(self.name)
         super(Menu, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    """
+    Creates catgegories for menu items
+    """
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
 
     def __str__(self):
         return self.name

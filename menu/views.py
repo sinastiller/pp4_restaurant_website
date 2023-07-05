358aaec570acc5be378d5d2cbdb0a6e2c0a8185e
sinastiller <sinastiller@gmx.de>
2023-07-05 14:06:25 +0000

    Add categories to menu_list method

--- menu/views.py
+++ menu/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from .models import Menu
+from .models import Menu, Category
 
 
 def menu_list(request):
     menu_list = Menu.objects.all()
+    categories = Category.objects.all()
 
     context = {
         'menu_list': menu_list,
+        'categories': categories,
     }
 
     return render(request, '../templates/menu.html', context)

4b114593be3049faaa365218cad8f44b16ccd6c3
sinastiller <sinastiller@gmx.de>
2023-07-05 10:52:52 +0000

    Add menu_list method

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Menu
 
-# Create your views here.
+
+def menu_list(request):
+    menu_list = Menu.objects.all()
+
+    context = {
+        'menu_list': menu_list,
+    }
+
+    return render(request, '../templates/menu.html', context)

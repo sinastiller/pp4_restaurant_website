1ddefb3496a44b4a45621102f93048fdd24e1a1b
sinastiller <sinastiller@gmx.de>
2023-07-07 10:26:30 +0000

    Add url path for table_booking

--- restaurant_website/urls.py
+++ restaurant_website/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexPage.as_view(), name='index'),
     path('menu_list/', include('menu.urls', namespace='menu_list')),
+    path('table_booking/', include('booking.urls', namespace='table_booking')),
     path('accounts/', include('allauth.urls')),
 ]

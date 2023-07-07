6b91d4c3ca5810ea95e2b4f3daf78fc1400fb4b0
sinastiller <sinastiller@gmx.de>
2023-07-07 15:43:38 +0000

    Import MinValueValidator, MaxValueValidator

--- booking/models.py
+++ booking/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Booking(models.Model):
@@ -7,7 +8,8 @@ class Booking(models.Model):
     """
     name = models.CharField(max_length=30)
     phone = models.IntegerField()
-    number_of_guests = models.IntegerField()
+    number_of_guests = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(4)])
     date = models.DateField()
     time = models.TimeField()
 

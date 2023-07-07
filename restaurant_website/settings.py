8ab05cecfc35dd4737755052d006ba118eebd023
sinastiller <sinastiller@gmx.de>
2023-07-07 10:24:57 +0000

    Add booking to installed apps

--- restaurant_website/settings.py
+++ restaurant_website/settings.py
@@ -42,6 +42,7 @@ ALLOWED_HOSTS = ['restaurant-website-ee029a10692a.herokuapp.com',
 INSTALLED_APPS = [
     'restaurant',
     'menu',
+    'booking',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

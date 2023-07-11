4c7b2983ddc9366cd76267358ce9ea86241e5fd8
sinastiller <sinastiller@gmx.de>
2023-07-11 14:15:42 +0000

    Add input requirements

--- booking/forms.py
+++ booking/forms.py
@@ -7,20 +7,32 @@ class BookingForm(forms.ModelForm):
     Table booking form
     """
 
-    date = forms.DateField(
-        input_formats=['%d/%m/%Y'],
-        widget=forms.DateTimeInput(attrs={
-            'placeholder': 'dd/mm/yyyy',
-        }))
+    number_of_guests = forms.IntegerField(required=True,
+                                          widget=forms.TextInput(
+                                              attrs={
+                                                  'pattern': '[1-4]+',
+                                                  'title': 'Enter a number between 1 and 4.'
+                                              }
 
-    phone = forms.CharField(
-        max_length=10,
-        widget=forms.TextInput(
-            attrs={
-                'placeholder': '0123456789',
-                }
-            )
-        )
+                                          ))
+    date = forms.DateField(input_formats=['%d/%m/%Y'],
+                           widget=forms.DateTimeInput(attrs={
+                               'placeholder': 'dd/mm/yyyy',
+                               'type': 'date-local'
+                           }))
+
+    name = forms.CharField(required=True, widget=forms.TextInput(
+        attrs={'pattern': '[A-Za-z ]+', 'title': 'Enter characters only.'}))
+
+    phone = forms.IntegerField(required=True,
+                               widget=forms.TextInput(
+                                   attrs={
+                                       'placeholder': '0123456789',
+                                       'pattern': '[0123456789]+',
+                                       'title': 'Enter digits only.'
+                                   }
+                               )
+                               )
 
     class Meta:
         model = Booking

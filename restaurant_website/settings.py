af279bf45e9b383d53ab27081904475a60b0adb0
sinastiller <sinastiller@gmx.de>
2023-07-17 09:38:28 +0000

    Set debug to false

--- restaurant_website/settings.py
+++ restaurant_website/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['restaurant-website-ee029a10692a.herokuapp.com',
                  '8000-sinastiller-pp4restaura-4dmd1gro0s9.ws-eu100.gitpod.io',
